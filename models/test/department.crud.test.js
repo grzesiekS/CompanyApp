1a2c11cd1f30cb584c601a06acb4c006f9d3e987
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-25 12:02:05 +0200

    Add test for Creating data (Department)

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -46,4 +46,18 @@ describe('Department', () => {
             await Department.deleteMany();
         });
     });
+
+    describe('Creating data', () => {
+        
+        it('should insert new document with "insertOne" method', async () => {
+            const department = new Department({ name: 'Department #1' });
+            await department.save();
+
+            expect(department.isNew).to.be.false;
+        });
+
+        after(async () => {
+            await Department.deleteMany();
+        });
+    });
 });
