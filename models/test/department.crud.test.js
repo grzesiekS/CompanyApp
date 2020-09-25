7148e97678623d844f1418cf5bf684eccf5964a9
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-25 11:42:22 +0200

    Fix error with connection

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -9,7 +9,7 @@ describe('Department', () => {
         try {
           const fakeDB = new MongoMemoryServer();
       
-          const uri = await fakeDB.getConnectionString();
+          const uri = await fakeDB.getUri();
       
           mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
       
@@ -17,5 +17,23 @@ describe('Department', () => {
           console.log(err);
         }
       
+    });
+
+    describe('Reading data', () => {
+
+        before(async () => {
+            const testDepOne = new Department({ name: 'Department #1' });
+            await testDepOne.save();
+        
+            const testDepTwo = new Department({ name: 'Department #2' });
+            await testDepTwo.save();
+        });
+
+        it('should return all the data with "find" method', async () => {
+            const departments = await Department.find();
+            const expectLength = 2;
+
+            expect(departments.length).to.be.equal(expectLength);
+        });
     });
 });
